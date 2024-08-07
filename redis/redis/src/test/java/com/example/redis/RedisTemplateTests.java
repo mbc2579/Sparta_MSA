5c0ddafaeaafe859870cb5137af8d7c6186e00d6
mbc2579 <mbc2579@naver.com>
2024-08-07 12:54:08 +0900

    Spring-redis-RedisTemplate

--- redis/redis/src/test/java/com/example/redis/RedisTemplateTests.java
+++ redis/redis/src/test/java/com/example/redis/RedisTemplateTests.java
@@ -1,8 +1,10 @@
 package com.example.redis;
 
+import com.example.redis.repository.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -30,5 +32,18 @@ public class RedisTemplateTests {
 
         stringRedisTemplate.expire("hobbies", 10, TimeUnit.SECONDS);
         stringRedisTemplate.delete("simplekey");
+    }
+
+    @Autowired
+    private RedisTemplate<String, ItemDto> itemRedisTemplate;
+
+    @Test
+    public void itemRedisTemplateTest() {
+        ValueOperations<String, ItemDto> ops = itemRedisTemplate.opsForValue();
+        ops.set("my:keyboard", ItemDto.builder().name("Mechanical Keyboard").price(250000).description("OMG").build());
+        System.out.println(ops.get("my:keyboard"));
+
+        ops.set("my:mouse", ItemDto.builder().name("mouse mice").price(100000).description("OMG").build());
+        System.out.println(ops.get("my:mouse"));
     }
 }
