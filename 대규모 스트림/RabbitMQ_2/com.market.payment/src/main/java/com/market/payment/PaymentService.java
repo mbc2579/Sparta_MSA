a68d1f3bc9104b7ab68c3c292683144959189380
mbc2579 <mbc2579@naver.com>
2024-08-19 11:20:44 +0900

    RabbitMQ-SAGA Pattern-Payment error

--- 대규모 스트림/RabbitMQ_2/com.market.payment/src/main/java/com/market/payment/PaymentService.java
+++ 대규모 스트림/RabbitMQ_2/com.market.payment/src/main/java/com/market/payment/PaymentService.java
@@ -2,6 +2,8 @@ package com.market.payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,6 +13,11 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PaymentService {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${message.queue.err.product}")
+    private String productErrorQueue;
+
     public void creatPayment(DeliveryMessage deliveryMessage) {
         Payment payment = Payment.builder().paymentId(UUID.randomUUID()).payAmount(deliveryMessage.getPayAmount()).userId(deliveryMessage.getUserId()).payStatus("SUCCESS").build();
 
@@ -18,6 +25,13 @@ public class PaymentService {
 
         if(payAmount >= 10000) {
             log.error("Payment amount exceeds limit : {}", payAmount);
+            deliveryMessage.setErrorType("PAYMENT_LIMIT_EXCEED");
+            this.rollbackPayment(deliveryMessage);
         }
+    }
+
+    public void rollbackPayment(DeliveryMessage deliveryMessage) {
+        log.info("PAYMENT ROLLBACK !!!");
+        rabbitTemplate.convertAndSend(productErrorQueue, deliveryMessage);
     }
 }
