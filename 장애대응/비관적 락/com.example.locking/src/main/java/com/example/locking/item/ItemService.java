43b13690e782c6909da81a3d19bec02da8a8e3ea
mbc2579 <mbc2579@naver.com>
2024-08-21 14:57:35 +0900

    장애대응-데드 락

--- 장애대응/비관적 락/com.example.locking/src/main/java/com/example/locking/item/ItemService.java
+++ 장애대응/비관적 락/com.example.locking/src/main/java/com/example/locking/item/ItemService.java
@@ -2,6 +2,7 @@ package com.example.locking.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -11,15 +12,31 @@ public class ItemService {
 
     @Transactional
     public void updateItemQuantity(Long itemId, Integer newQuantity) {
-        Item item = itemRepository.findByWithLock(itemId);
+        Item item = itemRepository.findByIdWithLock(itemId);
 
         item.setQuantity(newQuantity);
 
         itemRepository.save(item);
     }
 
-    @Transactional
+    @Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
     public Item findItemById(Long itemId) {
         return itemRepository.findById(itemId).orElse(null);
+    }
+
+    public void updateItemsQuantity(Long itemId1, Long itemId2) {
+        Item item1 = itemRepository.findByIdWithLock(itemId1);
+        item1.setQuantity(item1.getQuantity() + 10);
+        itemRepository.save(item1);
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Item item2 = itemRepository.findByIdWithLock(itemId2);
+        item2.setQuantity(item2.getQuantity() + 10);
+        itemRepository.save(item2);
     }
 }
